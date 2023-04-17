c6b0402ce264aaf1837363c96f7aa74a4c980b8b
AbdElrahman AbuHasan <abdabhasan@gmail.com>
2023-04-17 14:00:57 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import {
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./components/index";
+import styles from "./styles";
+import "./index.scss";
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+          </div>
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import styles from "../../styles";
+import "./Button.scss";
+
+const Button = ({ styles }) => {
+  return (
+    <button
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
+    >
+      Get Started
+    </button>
+  );
+};
+
+export default Button;
